8d1b545ea186ccc324562f14cf85d4d22be5050a
Brandon Jordan <brandonjordan124@gmail.com>
2025-04-04 18:12:16 -0400

    Update preview.js

--- assets/preview.js
+++ assets/preview.js
@@ -6,12 +6,19 @@ window.onload = () => {
     setTimeout(() => {
         const codes = document.querySelectorAll('.language-ruby, .language-javascript');
         codes.forEach(highlight => {
+            const encodedSrc = encodeURIComponent(highlight.innerText)
+            
             const iframe = document.createElement('iframe');
             iframe.frameBorder = '0';
             iframe.loading = 'lazy';
             iframe.title = 'Shortcut Preview';
-            iframe.src = 'https://playground.cherrilang.org/preview?code=' + encodeURIComponent(highlight.innerText);
+            iframe.src = 'https://playground.cherrilang.org/preview?code=' + encodedSrc;
             highlight.appendChild(iframe);
+            
+            const tryMe = document.createElement('a');
+            tryMe.href = 'https://playground.cherrilang.org/?code=' + encodedSrc;
+            tryMe.innerHTML = 'Try Me!';
+            highlight.appendChild(tryMe);
         });
     }, 700);
 };
