54f86ac004f99789df9ae9ee1d6db09b03e792a0
Brandon Jordan <brandon@cherrilang.org>
2025-10-14 23:46:45 -0400

    Update preview.js

--- assets/preview.js
+++ assets/preview.js
@@ -3,38 +3,55 @@
  */
 
 window.onload = () => {
-    loadPreviews()
+  const codes = document.querySelectorAll(
+    ".language-ruby, .language-javascript"
+  );
+  codes.forEach((highlight) => {
+    const encodedSrc = encodeURIComponent(highlight.innerText);
+    let frameURL =
+      "https://playground.cherrilang.org/preview?code=" + encodedSrc;
+
+    if (highlight.classList.contains("language-javascript")) {
+      frameURL += "&header=1";
+    }
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "code-example";
+
+    const showPreview = document.createElement("button");
+    showPreview.classList.add("btn", "btn-red", "btn-preview");
+    showPreview.innerHTML = "&#x25B6;";
+    showPreview.onclick = () => {
+      const iframe = document.createElement("iframe");
+      iframe.frameBorder = "0";
+      iframe.loading = "lazy";
+      iframe.src = frameURL;
+      iframe.title = "Shortcut Preview";
+
+      highlight.appendChild(iframe);
+    };
+
+    const tryMe = document.createElement("a");
+    tryMe.href = "https://playground.cherrilang.org/?code=" + encodedSrc;
+    tryMe.target = "_blank";
+    tryMe.classList.add("btn", "btn-red", "btn-playground");
+    tryMe.innerText = "Edit in Playground";
+    tryMe.onclick = () => {
+      const iframe = document.createElement("iframe");
+      iframe.frameBorder = "0";
+      iframe.loading = "lazy";
+      iframe.src = frameURL;
+      iframe.title = "Shortcut Preview";
+
+      highlight.appendChild(iframe);
+    };
+
+    const actions = document.createElement("div");
+    actions.className = "code-actions";
+    actions.append(showPreview, tryMe);
+
+    const parentElement = highlight.parentNode;
+    parentElement.insertBefore(wrapper, highlight);
+    wrapper.append(actions, highlight);
+  });
 };
-
-function loadPreviews() {
-    const codes = document.querySelectorAll('.language-ruby, .language-javascript');
-    codes.forEach(highlight => {
-        const encodedSrc = encodeURIComponent(highlight.innerText);
-        let frameURL = 'https://playground.cherrilang.org/preview?code=' + encodedSrc;
-
-        if (highlight.classList.contains('language-javascript')) {
-            frameURL += '&header=1';
-        }
-
-        const wrapper = document.createElement('div');
-        wrapper.className = 'code-example';
-
-        const tryMe = document.createElement('a');
-        tryMe.href = 'https://playground.cherrilang.org/?code=' + encodedSrc;
-        tryMe.target = '_blank';
-        tryMe.classList.add('btn', 'btn-red', 'm-3');
-        tryMe.innerHTML = 'Try Me!';
-
-        const iframe = document.createElement('iframe');
-        iframe.frameBorder = '0';
-        iframe.loading = 'lazy';
-        iframe.src = frameURL;
-        iframe.title = 'Shortcut Preview';
-
-        highlight.appendChild(iframe);
-
-        const parentElement = highlight.parentNode;
-        parentElement.insertBefore(wrapper, highlight);
-        wrapper.append(highlight, tryMe);
-    });
-}
